ad24da091d8bed874985d9028dfcf431ce331938
Sahil Lather <sahillather002@gmail.com>
2023-05-27 11:06:37 +0530

    Create sol.cpp

---
+++ 3. Sudoku Solver/sol.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h> 
+bool isSafe(int row,int col,vector<vector<int>>& board,int val){
+    for(int i=0;i<board.size();i++){
+        if(board[row][i]==val)
+            return false;
+        if(board[i][col]==val)
+            return false;
+        //3*3 check
+        if(board[3*(row/3)+i/3][3*(col/3)+i%3]==val)
+            return false;
+    }
+    return true;
+}
+bool solve(vector<vector<int>>& board){
+    int n=board[0].size();
+    for(int row=0;row<n;row++){
+        for(int col=0;col<n;col++){
+            if(board[row][col]==0){
+                for(int val=1;val<10;val++){
+                  if (isSafe(row, col, board, val)) {
+                    board[row][col] = val;
+                    // recursion
+                    bool ifNextPossible = solve(board);
+                    if (ifNextPossible)
+                      return true;
+                    else
+                      // backtrack
+                      board[row][col] = 0;
+                    }
+                } 
+                return false;
+            }
+        }
+    }
+    return true;
+}
+void solveSudoku(vector<vector<int>>& sudoku)
+{
+	// Write your code here
+    // No need to print the final sudoku
+    // Just fill the values in the given matrix
+    solve(sudoku);
+}
